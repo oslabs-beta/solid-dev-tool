1cd4e7413c34a19c34235179bd56d52b572e3ba7
Hulkaroy Jouraboeva <86935670+hulkaroyJouraboeva@users.noreply.github.com>
2022-08-23 11:33:40 -0400

    Drawer complete with dragging/resizing feature

--- solid-dev-tool/src/Panel.tsx
+++ solid-dev-tool/src/Panel.tsx
@@ -1,15 +1,9 @@
 import { Component, createEffect, createRoot, createSignal, getOwner, Show, JSX, onMount, onCleanup } from "solid-js"
-import { render } from "solid-js/web";
-import "solid-bottomsheet/styles.css";
-import { SolidBottomsheet } from 'solid-bottomsheet';
+import { SolidBottomsheet } from './SolidDrawer';
 import SignalList from "./SignalList";
 
 export default function Panel(props) {
-  const [clicked, setClicked] = createSignal(false);
   const [isOpen, setOpen] = createSignal(false);
-  // const [isResizing, setResizing] = createSignal(false);
-  // const [lastDownX, setLastDownX] = createSignal(0);
-  // const [newWidth, setNewWidth] = createSignal({});
 
   onMount(() => setOpen(true));
 
@@ -21,7 +15,6 @@ export default function Panel(props) {
           defaultSnapPoint={({ maxHeight }) => maxHeight / 3}
           snapPoints={({ maxHeight }) => [maxHeight, maxHeight / 4]}
           onClose={() => setOpen(!isOpen())}
-          // className="drawer"
         >
             <SignalList root={props.root} />
         </SolidBottomsheet>
