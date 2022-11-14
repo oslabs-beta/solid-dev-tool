d35d866c4357a3473b060fa1f4cd86b18f15204f
lnhunt11 <86940809+lnhunt11@users.noreply.github.com>
2022-11-14 14:21:38 -0700

    added panel display test

--- solid-dev-tool/cypress/e2e/solid-dev-tool.cy.js
+++ solid-dev-tool/cypress/e2e/solid-dev-tool.cy.js
@@ -4,6 +4,9 @@
  * below are the test cases
 */
 
+// for mor info on Cypress api:
+// https://docs.cypress.io/api/commands/get#Syntax
+
 describe('e2e testing of visualizer', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
@@ -12,14 +15,34 @@ describe('e2e testing of visualizer', () => {
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('/')
   })
-  it('displays floating action button by default', () => {
 
+  it('displays list by default with closed button', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert there is a class showing a closed button
-    cy.get('[data-testid="fab"]').should('have.class', 'stf closed')
-
-
+    cy.get('[data-testid="fab"]').should('not.contain', 'stf closed')
   })
+
+  it('panel should not be open as default', () => {
+    cy.get('[data-testid="fab"]').should('not.contain', 'footer')
+  })
+
+  it('main action button should open when clicked on', () => {
+    cy.get('[data-testid="fab"]').click()
+    .should('have.class', 'stf open')
+    .get('li.stf-mb_c')
+    .find('button')
+    .should('have.class', 'stf--mb')
+  })
+
+  it('once you click action button panel should display', () => {
+    cy.get('[data-testid="fab"]').click()
+    .should('have.class', 'stf open')
+    .get('li.stf-mb_c')
+    .find('button')
+    .click({ multiple: true })
+    cy.get('footer').should('have.id', 'Panel')
+  })
+
 
 
 })
