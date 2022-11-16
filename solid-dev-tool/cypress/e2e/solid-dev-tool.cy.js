569fa5cb88dcb5f9f03ca384628a012c2102f877
lnhunt11 <86940809+lnhunt11@users.noreply.github.com>
2022-11-16 14:43:05 -0700

    tested floating action button for opening and closing

--- solid-dev-tool/cypress/e2e/solid-dev-tool.cy.js
+++ solid-dev-tool/cypress/e2e/solid-dev-tool.cy.js
@@ -7,7 +7,7 @@
 // for mor info on Cypress api:
 // https://docs.cypress.io/api/commands/get#Syntax
 
-describe('e2e testing of visualizer', () => {
+describe('testing action buttons', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
@@ -34,15 +34,43 @@ describe('e2e testing of visualizer', () => {
     .should('have.class', 'stf--mb')
   })
 
-  it('once you click action button panel should display', () => {
+
+  it('Floating action button should close the panel when clicked on twice', () => {
+    cy.get('[data-testid="fab"]')
+    .click({ multiple: true })
+    .get('li.stf-mb_c')
+    .find('button')
+    .click({ multiple: true })
+    .get('[data-testid="fab"]')
+    .click({ multiple: true })
+    .find('li.stf-mb_c')
+    .find('button')
+    .click({ multiple: true })
+    .get('[data-testid="fab"]')
+    .should('have.class', 'stf closed')
+    cy.get('[data-testid="fab"]').should('not.contain', 'footer')
+  })
+})
+
+describe('testing panel where reactive graph is displayed', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('/')
     cy.get('[data-testid="fab"]').click()
     .should('have.class', 'stf open')
     .get('li.stf-mb_c')
     .find('button')
     .click({ multiple: true })
+  })
+
+  it('Once you click action button panel should display', () => {
     cy.get('footer').should('have.id', 'Panel')
   })
 
-
+  // it('Panel should have a border that can be dragged')
 
 })
+
