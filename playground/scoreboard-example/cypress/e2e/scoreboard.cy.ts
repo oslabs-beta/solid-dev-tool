10f6cc5c07d227f9936349f3e06125d302170cdc
lnhunt11 <86940809+lnhunt11@users.noreply.github.com>
2022-11-20 20:18:07 -0700

    added testing settings in example

--- playground/scoreboard-example/cypress/e2e/scoreboard.cy.ts
+++ playground/scoreboard-example/cypress/e2e/scoreboard.cy.ts
@@ -8,4 +8,9 @@ describe('example to-do app', () => {
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('/')
   })
+  it('Checking if default names render', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert there is a class showing a closed button
+    cy.get('form').should('have.class', 'admin')
+  })
 })
